b7b04a4ce0d04cae02a6faf469d67264c46b1ee9
ralves <alves.rodrigo31@outlook.com>
2017-11-28 09:45:44 -0200

    Iniciando o projeto

---
+++ cms/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Blog</title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://bootswatch.com/3/journal/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<!-- Estilos -->
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	
+		<!-- Menu -->
+		
+			
+				
+	<nav class="navbar navbar-dark navbar-fixed-top">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span> 
+				</button>
+				<a class="navbar-brand" href="#">Título da Página</a>
+			</div>
+			<div class="collapse navbar-collapse pull-right" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li class="nav-item"><a href="#">Noticias</a></li>
+					<li class="nav-item"><a href="#">Esporte</a></li>
+					<li class="nav-item"><a href="#">Tecnologia</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	
+	
+
+
+	<div class="jumbotron">
+	    <h1>Bootstrap Tutorial</h1> 
+	    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing
+	    responsive, mobile-first projects on the web.</p> 
+	</div>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8">
+				Teste
+			</div>
+			<div class="col-lg-4">
+				Teste
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html>
